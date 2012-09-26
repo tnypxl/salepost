204c936db44deb2a2815d4cdb3f61fca79550641
Arik Jones <arik.jones@kearneyhub.com>
2012-09-26 13:54:04 -0500

    Database migration

---
+++ db/migrate/20120926183726_create_posts.rb
@@ -0,0 +1,12 @@
+class CreatePosts < ActiveRecord::Migration
+  def change
+    create_table :posts do |t|
+      t.string :title
+      t.text :description
+      t.string :city
+      t.string :state
+
+      t.timestamps
+    end
+  end
+end
